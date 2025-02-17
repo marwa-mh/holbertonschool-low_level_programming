f236b0da8915ec839d637efc1f17648f58a9c4d4
marwa <marwa.almahmoud@hotmail.com>
2025-02-17 01:51:10 +0000

    Add 4-puts file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Uses puts to print a text
+ * Return: 0 for success
+ */
 int main(void)
 {
-	/*
-	 * Print a sentense using puts function
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
