3cfdb26ed1e349dbe9a18a7d49d509e5f92683d7
marwa <marwa.almahmoud@hotmail.com>
2025-02-17 03:58:05 +0000

    Add 6-size file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 
-/*
- * This is a project that print the sizes
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the size of various data types on the system
+ * where the program is compiled and executed.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-	/*
-	 * This is the main function
-	 */
+{	
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
