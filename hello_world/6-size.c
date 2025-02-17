85017804a562daf483ae04513f6b27c1adf60dd0
marwa <marwa.almahmoud@hotmail.com>
2025-02-17 04:01:15 +0000

    Add 6-size file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
  * main - Entry point of the program
  *
@@ -9,7 +8,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
