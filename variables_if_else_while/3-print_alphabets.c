2302bf12071e8c60141c05730ee70399575b1b47
Marwa <marwa.almahmoud@hotmail.com>
2025-02-18 04:39:05 +0000

    Add 3-print_alphabets.c file

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Uses puts to print a text
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int asciCode = 97;
+
+	while (asciCode <= 122)
+	{
+		putchar(asciCode);
+		asciCode++;
+	}
+	asciCode = 65;
+
+	while (asciCode <= 90)
+	{
+		putchar(asciCode);
+		asciCode++;
+	}
+	putchar('\n');
+
+	return (0);
+}
