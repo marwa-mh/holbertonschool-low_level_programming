18066ee0496637a7ffd1efa34b1b72d95928203c
Marwa <marwa.almahmoud@hotmail.com>
2025-02-18 04:58:08 +0000

    Add 5=print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Uses puts to print a text
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
