d18f38f8da91861dcb0e4a46bcb750a03a1e6e11
Marwa <marwa.almahmoud@hotmail.com>
2025-02-19 05:16:33 +0000

    Add 7 print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of the number
+ *@n: input number
+ * Return: int
+ *
+ */
+int print_last_digit(int n)
+{
+	int lastDigit = n % 10;
+
+	_putchar(lastDigit);
+	return (lastDigit);
+}
