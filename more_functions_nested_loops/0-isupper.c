50f26543533e43990370ed05411e16d3b0755b7a
Marwa <marwa.almahmoud@hotmail.com>
2025-02-23 20:27:44 +0000

    Add 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * _isupper - check the character if in upper case
+ * @c: input character
+ * Return int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
