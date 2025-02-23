ca03223414a9363e1d14b2896d7451f4c2de86d3
Marwa <marwa.almahmoud@hotmail.com>
2025-02-23 23:23:16 +0000

    Add 3-print-numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - multiply two integer
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	char ch = 48;
+
+	while (ch <= 57)
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
