bd17a6ecda2866cda428253e2afb4b3d95599cba
Marwa <marwa.almahmoud@hotmail.com>
2025-02-24 00:45:05 +0000

    Add 8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: input number
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int width = 1;
+	int length = 1;
+
+	while (width <= size)
+	{
+		while (length <= size)
+		{
+			_putchar('#');
+			length++;
+		}
+		length = 1;
+		width++;
+		_putchar('\n');
+	}
+}
