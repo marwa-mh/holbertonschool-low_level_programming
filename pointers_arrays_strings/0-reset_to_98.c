ff13ff79067455324b848cf4fc0e168b4a17205e
Marwa <marwa.almahmoud@hotmail.com>
2025-02-25 01:32:22 +0000

    Add 0-reset_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - multiply two integer
+ * @n: input
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/func_template.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integer
+ * @n: input
+ * Return: void
+ */
+void mul(int n)
+{
+	return (0);
+}
