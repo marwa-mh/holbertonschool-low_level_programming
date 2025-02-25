34e6859e020ddc0a14a8b005c75d366023d084a3
Marwa <marwa.almahmoud@hotmail.com>
2025-02-25 02:38:42 +0000

    Add 4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse followed by new line
+ * @s: input
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int str_length = 0;
+
+	while (*s != '\0')
+	{
+		str_length++;
+		s++;
+	}
+	while (str_length > 0)
+	{
+		_putchar(*s);
+		str_length--;
+		s--;
+	}
+	_putchar('\n');
+}
