fe511fc0e6a13ce6323892625b3ac4ffae30e34c
Marwa <marwa.almahmoud@hotmail.com>
2025-02-28 04:32:04 +0000

    Add print number

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -12,9 +12,9 @@ void print_number(int n)
 		_putchar('-');
 		n = n * -1;
 	}
-	if (number >= 10)
+	if (n >= 10)
 	{
-		_print_number(n / 10);
+		print_number(n / 10);
 		_putchar(48 + n % 10);
 	}
 	else
