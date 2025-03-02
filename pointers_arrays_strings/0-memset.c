c0fe6a90bb21a30173b2622787ce18597a7805ea
Marwa <marwa.almahmoud@hotmail.com>
2025-03-02 19:47:05 +0000

    Add 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_memset - fills the memory with constant byte
+ * @n: fist n bytes of the memory area
+ * @s: string
+ * @b: constant byte
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *ps = s;
+
+	i = 0;
+	while (i < n && *s !='\0')
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+	return ps;
+}
