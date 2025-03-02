90a6176e30bb34464742b3d403636aeb9b7cea36
Marwa <marwa.almahmoud@hotmail.com>
2025-03-02 20:18:33 +0000

    Add 1-memcpy.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,4 +21,4 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char *src, unsigned int c);
+char *_memcpy(char *dest, char *src, unsigned int c);
