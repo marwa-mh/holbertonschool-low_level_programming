4f311ed7cc93d0b42d55786908f13f63aadcb5dc
Marwa <marwa.almahmoud@hotmail.com>
2025-03-03 04:14:13 +0000

    Add 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *_strstr - locate a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *p_needle = needle;
+	char *target;
+	int target_found = 0;
+
+	while (*p_needle != '\0')
+	{
+		while (*haystack != '\0')
+		{
+			if (*p_needle == *haystack)
+			{
+				if (target_found == 0)
+				{
+					target = *haystack;
+					target_found = 1;
+				}
+				p_needle++;
+			}
+			else if (p_needle != needle)
+			{
+				p_needle = needle;
+				target = '\0';
+				target_found = 0;
+			}
+			haystack++;
+		}
+		if (target_found == 0)
+			return (NULL);
+	}
+	return (target);
+}
