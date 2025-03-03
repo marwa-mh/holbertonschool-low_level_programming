4f97616928f7ba47f832e806259df8d1b4e48fcc
Marwa <marwa.almahmoud@hotmail.com>
2025-03-03 04:27:23 +0000

    Add 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,29 +12,25 @@ char *_strstr(char *haystack, char *needle)
 	char *target;
 	int target_found = 0;
 
-	while (*p_needle != '\0')
+	while (*haystack != '\0' && *p_needle != '\0')
 	{
-		while (*haystack != '\0')
+		if (*p_needle == *haystack)
 		{
-			if (*p_needle == *haystack)
+			if (target_found == 0)
 			{
-				if (target_found == 0)
-				{
-					target = haystack;
-					target_found = 1;
-				}
-				p_needle++;
+				target = haystack;
+				target_found = 1;
 			}
-			else if (p_needle != needle)
-			{
-				p_needle = needle;
-				target = '\0';
-				target_found = 0;
-			}
-			haystack++;
+			p_needle++;
 		}
-		if (target_found == 0)
-			return (NULL);
+		else if (p_needle != needle)
+		{
+			p_needle = needle;
+			target_found = 0;
+		}
+		haystack++;
 	}
-	return (target);
+	if (target_found == 0)
+		return (NULL);
+return (target);
 }
