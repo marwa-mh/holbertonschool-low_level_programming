579be33b7a0f62d2589addb9a4d096d088c972bb
Marwa <marw.almahmoud@hotmail.com>
2025-03-05 01:09:55 +0000

    Add 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - check the code
+ *@argc: count of argument
+ *@argv: array of arrgument
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int count = 1;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (count < argc)
+	{
+		if (isdigit(*argv[count]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	/*	if (*argv[count] != '0' && isdigit(argv[count]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}*/
+		result += atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n", result);
+	return (0);
+}
