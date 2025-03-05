bde463998246815a7cadfad103e06ef919ce39a1
Marwa <marw.almahmoud@hotmail.com>
2025-03-05 02:33:59 +0000

    Add 100-change cent

---
+++ argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *@argc: count of argument
+ *@argv: array of arrgument
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int number = 0;
+	int result = 0;
+	int i = 0;
+	int change[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	number = atoi(argv[1]);
+	if (number <= 0)
+		printf("0\n");
+	while (number != 0)
+	{
+		for (i = 0; i < 5; i++)
+			if (number - change[i] >= 0)
+			{
+				number -= change[i];
+				result++;
+				break;
+			}
+	}
+	printf("%d\n", result);
+	return (0);
+}
