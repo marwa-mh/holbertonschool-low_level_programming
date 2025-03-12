0aec8c034ec7928968642ec2bbec33a21ce488a3
Marwa <marwa.almahmoud@hotmail.com>
2025-03-12 22:41:18 +0000

    add 3-dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _dog_h_
-#define _dog_h_
+#ifndef _dog_t_h_
+#define _dog_t_h_
 /**
  * struct dog - structure
  * @name: name
@@ -14,4 +14,16 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * struct dog_t - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
 #endif
