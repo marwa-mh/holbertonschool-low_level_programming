cdc0c1ccb8c7176dd82ceab7f19b2b665bfaa083
Marwa <marwa.almahmoud@hotmail.com>
2025-03-12 05:55:52 +0000

    Add 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,6 +14,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+	if (nmemb > (unsigned int) -1 / size);
+	return (NULL);
 	array = malloc(nmemb * size);
 	if (array == NULL)
 		return (NULL);
