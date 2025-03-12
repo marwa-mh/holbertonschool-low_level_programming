1b920a05d32f48f7ccf5054fb4764a655055824d
Marwa <marwa.almahmoud@hotmail.com>
2025-03-12 19:54:23 +0000

    Add dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
