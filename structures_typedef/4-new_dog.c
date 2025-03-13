48be2be1201982f10ffca53fb0ef8eea7f7be38b
Marwa <marwa.almahmoud@hotmail.com>
2025-03-13 01:26:21 +0000

    Add 4-new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * cpstr - copy string
+ * @str: input
+ * Return: string
+ */
+char *cpstr(char *str)
+{
+	char *p_str = str;
+	char *newstr;
+	int size = 0, i = 0;
+
+	while (*str != '\0')
+	{
+		size++;
+		str++;
+	}
+	size++;
+	newstr = malloc(sizeof(char) * size);
+	if (newstr == NULL)
+	{
+		free(newstr);
+		return (NULL);
+	}
+	str = p_str;
+	while (i < size)
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	return (newstr);
+}
+/**
+ * new_dog - create new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = cpstr(name);
+	dog->age = age;
+	dog->owner = cpstr(owner);
+	return (dog);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,4 +15,5 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
