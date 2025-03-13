bca058ecc6797cfd3149806e0a9a2a888f389657
Marwa <marwa.almahmoud@hotmail.com>
2025-03-13 19:21:36 +0000

    Add 5-free

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,11 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * free-dog - free dog
+ * free_dog - free dog
  * @d: struct
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);
