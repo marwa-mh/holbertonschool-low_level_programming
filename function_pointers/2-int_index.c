7bd64fd69cb5cba423c40e7e49566a953a44aad4
marwa <marwa.almahmoud@hotmail.com>
2025-03-17 02:30:50 +0000

    Add 2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * int_index - return the index of the first element
+ * @array: array of element
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int (*fp)(int);
+	int i = 0;
+
+	fp = cmp;
+	for (i = 0; i < size; i++)
+		if (cmp(array[i] != 0)
+			return (i);
+	return (i);
+}
