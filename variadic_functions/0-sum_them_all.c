185faf164e327a237f013f5c26981983adec11c4
Marwa <marwa.almahmoud@hotmail.com>
2025-03-18 03:51:58 +0000

    Add betty

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdarg.h>
+/**
+ * sum_them_all - sm them all
+ * @n: numbers
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	int i;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,4 @@
+#ifndef _v_f_h
+#define _v_f_h
+int sum_them_all(const unsigned int n, ...);
+#endif
