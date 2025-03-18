9c3853712a34d40081fad7bc0aa6301705dc1e79
Marwa <marwa.almahmoud@hotmail.com>
2025-03-18 04:16:49 +0000

    Add betty

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdarg.h>
+/**
+ * print_numbers - print numbers with separator
+ * @separator: separator
+ * @n: size
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 #ifndef _v_f_h
 #define _v_f_h
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
