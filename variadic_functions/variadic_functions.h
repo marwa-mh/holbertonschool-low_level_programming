fb606380cb5b1ed35e01bf22cab96500d31046ec
Marwa <marwa.almahmoud@hotmail.com>
2025-03-19 01:52:33 +0000

    fix betty

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef _v_f_h
 #define _v_f_h
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
