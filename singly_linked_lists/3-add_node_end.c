d3ff2c9bc0191f98523ddbda9a152babbb773e14
Marwa <marwa.almahmoud@hotmail.com>
2025-04-01 04:33:10 +0000

    Add 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,6 +2,17 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+int _strlen(const char *str)
+{
+    int len= 0;
+
+    while(*str !='\0')
+    {
+    len++;
+    str++;
+    }
+    return len;
+}
 list_t *add_node_end(list_t **head, const char *str)
 {
 
@@ -11,7 +22,7 @@ list_t *add_node_end(list_t **head, const char *str)
   if(!new_node)
   return (NULL);
    new_node->str =strdup(str);
-   new_node->len=strlen(str);
+   new_node->len=_strlen(str);
    new_node->next = NULL;
 
    if (!*head)
