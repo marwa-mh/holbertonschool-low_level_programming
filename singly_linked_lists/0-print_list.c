a350e8507694588ca14aecc281c08c13a6455e2e
Marwa <marwa.almahmoud@hotmail.com>
2025-04-01 07:08:44 +0000

    Change betty style for all files

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,13 +13,11 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		count++;
-
 		h = h->next;
 	}
 	return (count);
