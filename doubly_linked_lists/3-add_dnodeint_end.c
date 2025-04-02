d3a67324a5c678748a283c6b606a5f0de98b68d2
Marwa <marwa.almahmoud@hotmail.com>
2025-04-02 02:00:36 +0000

    Add 3-add

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *ptr = *head;
-	
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
