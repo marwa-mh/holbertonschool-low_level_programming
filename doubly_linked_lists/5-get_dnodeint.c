01895a14c83ae41093dccdc515c01eabe02881ad
Marwa <marwa.almahmoud@hotmail.com>
2025-04-02 03:10:10 +0000

    Add 5-get_node by index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get node at index
+ * @index: index
+ * @head: points to the first node
+ *
+ * Return: point to the node
+ *
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int n = 0;
+
+	if (!head)
+		return (NULL);
+	while (n != index)
+	{
+		head = head->next;
+		n++;
+	}
+	return (head);
+}
