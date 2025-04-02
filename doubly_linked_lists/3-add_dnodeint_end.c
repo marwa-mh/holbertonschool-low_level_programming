5154b87de7bda90a7c605ad5a402777099994294
Marwa <marwa.almahmoud@hotmail.com>
2025-04-02 01:59:28 +0000

    Add 3-add

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * add_dnodeint - add node to the linked list
+ * add_dnodeint_end - add node to the linked list
  * @n: number
  * @head: points to the first node
  *
  * Return: point to the new node
  *
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+	dlistint_t *ptr = *head;
 	
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -20,15 +21,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		printf("Error\n");
 		return (NULL);
 	}
+	new_node->n = n;
 	if (!*head)
 	{
 		*head = new_node;
 	}
-	new_node->n = n;
-	printf("%d\n", new_node->n);
-	(*head)->prev = new_node;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	*head = new_node;
+	else
+	{
+		while ((*head)->next != NULL)
+		{
+			*head = (*head)->next;
+		}
+		(*head)->next = new_node;
+		new_node->prev = *head;
+		*head = ptr;
+	}
 	return (new_node);
 }
