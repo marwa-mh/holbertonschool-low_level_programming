feefb55a5b8775770ca1e2a8a74f557f478eb24d
Marwa <marwa.almahmoud@hotmail.com>
2025-04-03 04:14:31 +0000

    Add 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
 /**
@@ -10,19 +9,15 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-
 	unsigned int index = 0;
-	dlistint_t *tmpNode;
-	dlistint_t *ptr = *h;
+	dlistint_t *tmpNode, *ptr = *h;
 
+	tmpNode = malloc(sizeof(dlistint_t));
+	if (tmpNode == NULL)
+		return (NULL);
+	tmpNode->n = n;
 	if (ptr == NULL)
 	{
-		tmpNode = malloc(sizeof(dlistint_t));
-		if (tmpNode == NULL)
-			return (NULL);
-		tmpNode->n = n;
-		tmpNode->prev = NULL;
-		tmpNode->next = NULL;
 		(*h) = tmpNode;
 		return (tmpNode);
 	}
@@ -33,15 +28,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (idx > index + 1)
 		return (NULL);
-	tmpNode = malloc(sizeof(dlistint_t));
-	if (tmpNode == NULL)
-		return (NULL);
-	tmpNode->n = n;
-
 	if (index == 0)
 	{
 		tmpNode->next = ptr;
-		tmpNode->prev = NULL;
 		ptr->prev = tmpNode;
 		ptr = ptr->prev;
 		*h = ptr;
