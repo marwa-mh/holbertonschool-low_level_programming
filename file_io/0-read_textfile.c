f93a53fc8f8a89e4744deb8b85a6a65c3c3f5120
Marwa <marwa.almahmoud@hotmail.com>
2025-04-04 22:27:19 +0000

    Add 0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * read_textfile - read a file
+ * @filename: name of file
+ * @letters: the number of characters we want to read
+ * Return: number
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t r;
+	ssize_t w;
+	
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	r = read(fd, buffer, letters);
+	if (r == 0)
+		return (0);
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == 0)
+		return (0);
+	if (w != r)
+		return (0);
+	free(buffer);
+	close(fd);
+	return (w);
+}
+
