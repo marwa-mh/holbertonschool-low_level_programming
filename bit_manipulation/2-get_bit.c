33dc97926d494e118130229de76225422cfd18da
Marwa <marwa.almahmoud@hotmail.com>
2025-04-04 04:15:02 +0000

    add 2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - get bit depending on index
+ * @n: number
+ * @index: index
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(n) * 8))
+		return (-1);
+
+	return (n >> index) & 1;
+}
