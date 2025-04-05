d806767ee1f0563cb0c02cd349bc2f691ec889f9
Marwa <marwa.almahmoud@hotmail.com>
2025-04-05 06:58:43 +0000

    Add 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,32 +4,46 @@
 #include <unistd.h>
 /**
  * main - copies the content of one file to another.
- * @ac: argument count.
- * @av: argument vector.
+ * @argc: argument count.
+ * @argv: argument vector.
  * Return: 0 on success.
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
+	int fd_from, fd_to, r, w;
 	char buffer[1024];
-	ssize_t r, w;
-	int fd_from, fd_to;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(av[1], O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	r = read(fd_from, buffer, 1024);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
+		exit(99);
+	}
+	w = write(fd_to, buffer, r);
+	if (w == -1 || w != r)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		close(fd_to);
 		exit(99);
 	}
 	while ((r = read(fd_from, buffer, 1024)) > 0)
@@ -37,7 +51,7 @@ int main(int ac, char **av)
 		w = write(fd_to, buffer, r);
 		if (w == -1 || w != r)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -45,7 +59,7 @@ int main(int ac, char **av)
 	}
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
@@ -62,3 +76,4 @@ int main(int ac, char **av)
 	}
 	return (0);
 }
+
