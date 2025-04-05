6e6c0653a96453d78215c809c25de4c5db245008
Marwa <marwa.almahmoud@hotmail.com>
2025-04-05 07:53:53 +0000

    Handle betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,63 +15,49 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-		print_error(97, "Usage: cp file_from file_to\n","");
+		print_error(97, "Usage: cp file_from file_to\n", "");
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprint_error(98, "Error: Can't read from file %s\n", argv[1]);
 	r = read(fd_from, buffer, 1024);
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
-		exit(98);
 	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		print_error(99, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
-		exit(99);
 	}
 	w = write(fd_to, buffer, r);
 	if (w == -1 || w != r)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		print_error(99, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		close(fd_to);
-		exit(99);
 	}
 	while ((r = read(fd_from, buffer, 1024)) > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w == -1 || w != r)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			print_error(99, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
 		}
 	}
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(98);
 	}
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		print_error(100, "Error: Can't close fd %d\n", fd_from);
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		print_error(100, "Error: Can't close fd %d\n", fd_to);
 	return (0);
 }
 
